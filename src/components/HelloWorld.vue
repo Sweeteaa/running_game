4276328efcb66e32d806e9276a9283ed1de5915a
Sweeteaa <1668320581@qq.com>
2022-07-16 10:34:07 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,110 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class='wrap'>
+    <img class="user-img" ref="userImg" :src="userPic">
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'HelloWorld',
+  name: 'userModel',
   props: {
-    msg: String
+    speed: {
+      type: Number,
+      default: 1
+    },
+    status: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      userRunList: [],
+      slideList: [],
+      userPic: '',
+      standPic: require('@/assets/images/user/stand.png'),
+      userJumpPic: require('@/assets/images/user/run/Run_000.png'),
+      userSlidePic: require('@/assets/images/user/slide/Sliding_000.png'),
+      runInterval: null, // 跑步定时器
+      // 最后一次运动时间
+      lastRunTime: 0,
+    }
+  },
+  watch : {
+    status: {
+      handler(newVal) {
+        cancelAnimationFrame(this.runInterval)
+        this.$refs.userImg && this.$refs.userImg.classList.remove('slide', 'jump')
+        if (newVal === 'run') {
+          this.run()
+        } else if (newVal === 'slide') {
+          this.$refs.userImg.classList.add('slide')
+          this.slide()
+        } else if (newVal === 'jump') {
+          this.$refs.userImg.classList.add('jump')
+          this.jump()
+        } else {
+          this.userPic = this.standPic
+        }
+      },
+      immediate: true
+    }
+  },
+  mounted() {
+    for (let index = 0; index < 10; index++) {
+      let img = require('@/assets/images/user/run/Run_00'+index+'.png')
+      this.userRunList.push(img)
+    }
+    // this.run()
+  },
+  methods: {
+    // 动画
+    run() {
+      let _this = this
+      let _index = 0
+
+      let _run = () => {
+        let now = new Date().getTime()
+        if (now - this.lastRunTime > 120 - (this.speed * 20)) {
+          if (_index > (this.userRunList.length - 1) ) {
+            _index = 0
+          }
+          _this.userPic = this.userRunList[_index]
+          _index++
+          this.lastRunTime = now
+        }
+        this.runInterval = requestAnimationFrame(_run)
+      }
+      _run()
+    },
+    slide () {
+      this.userPic = this.userSlidePic
+    },
+    jump () {
+      this.userPic = this.userJumpPic
+    }
+    
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.wrap {
+  font-size: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.user-img {
+  width: 120px;
+  height: 208px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.user-img.slide {
+  width: 133.4px;
+  height: 163px;
 }
-a {
-  color: #42b983;
+.user-img.jump {
+  margin-bottom: 80px;
 }
 </style>
